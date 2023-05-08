9d3093d6f005439b78383257e9626f0bac4a3ffd
Shibaken28 <8128kanzensu@gmail.com>
2023-05-08 22:10:15 +0900

    feat: add unbalance RSA

---
+++ problems/Unbalance-RSA/encode.py
@@ -0,0 +1,19 @@
+from gmpy2 import iroot
+from Crypto.Util.number import long_to_bytes, bytes_to_long, getPrime
+
+m = bytes_to_long(b"sctf{sti11_we4k_rs@}")
+b = [25, 25, 25, 25, 25, 25, 25, 256, 256, 512] 
+ps = []
+e = 65537
+
+for a in b:
+    ps.append(getPrime(a))
+
+n = 1
+for p in ps:
+    n *= p
+
+c = pow(m, e, n)
+print(f"n = {n}")
+print(f"c = {c}")
+print(f"e = {e}")
